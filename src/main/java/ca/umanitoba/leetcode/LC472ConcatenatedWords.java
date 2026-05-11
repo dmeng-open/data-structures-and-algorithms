d143d33c75e020533c65394d821fcf8f406992ce
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-05-10 22:43:03 -0500

    472

--- src/main/java/ca/umanitoba/leetcode/LC472ConcatenatedWords.java
+++ src/main/java/ca/umanitoba/leetcode/LC472ConcatenatedWords.java
@@ -1,44 +1,64 @@
 package ca.umanitoba.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class LC472ConcatenatedWords {
     private static class TrieNode {
-        private boolean isWord;
+        private boolean ended;
         private TrieNode[] children = new TrieNode[26];
     }
 
     private TrieNode root = new TrieNode();
 
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, Comparator.comparingInt(String::length));
         List<String> result = new ArrayList<>();
-        for (String word : words) insert(word);
-        for (String word : words) if (search(word, 0, 0)) result.add(word);
+        for (String word : words) {
+            if (concatenatable(word, new Boolean[word.length()], 0)) {
+                result.add(word);
+            }
+            add(word);
+        }
         return result;
     }
 
-    private boolean search(String word, int start, int count) {
-        if (start == word.length() && count > 1) return true;
-        int size = word.length();
-        TrieNode node = root;
-        for (int i = start; i < size; i++) {
-            char character = word.charAt(i);
-            int index = character - 'a';
-            if (node.children[index] == null) return false;
-            node = node.children[index];
-            if (node.isWord && search(word, i + 1, count + 1)) return true;
+    private boolean concatenatable(String word, Boolean[] cache, int start) {
+        if (start == word.length()) {
+            return true;
         }
+        if (cache[start] != null) {
+            return cache[start];
+        }
+        TrieNode current = root;
+        for (int i = start; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                break;
+            }
+            current = current.children[index];
+            if (current.ended) {
+                if (concatenatable(word, cache, i + 1)) { // DFS 尝试不同路径 catsdog - { cat + sdog } = F | { cats + dog } = T
+                    cache[start] = true;
+                    return true;
+                }
+            }
+        }
+        cache[start] = false;
         return false;
     }
 
-    private void insert(String word) {
-        TrieNode node = root;
+    private void add(String word) {
+        TrieNode current = root;
         for (char character : word.toCharArray()) {
             int index = character - 'a';
-            if (node.children[index] == null) node.children[index] = new TrieNode();
-            node = node.children[index];
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
         }
-        node.isWord = true;
+        current.ended = true;
     }
 }

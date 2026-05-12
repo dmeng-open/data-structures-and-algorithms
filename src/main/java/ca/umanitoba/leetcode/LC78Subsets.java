06fee0950f6a07d9ca73bba792a11db392f4cb78
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-05-11 22:08:45 -0500

    78

---
+++ src/main/java/ca/umanitoba/leetcode/LC78Subsets.java
@@ -0,0 +1,23 @@
+package ca.umanitoba.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class LC78Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, result, new ArrayDeque<>(), 0);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, Deque<Integer> path, int start) {
+        result.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++) {
+            path.addLast(nums[i]);
+            backtrack(nums, result, new ArrayDeque<>(), i + 1);
+            path.removeLast();
+        }
+    }
+}

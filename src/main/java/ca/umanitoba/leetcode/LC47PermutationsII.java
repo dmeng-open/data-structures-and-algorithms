9cf0577ac03794297af50f078b83dfed65e6573b
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-05-12 20:04:24 -0500

    47

---
+++ src/main/java/ca/umanitoba/leetcode/LC47PermutationsII.java
@@ -0,0 +1,29 @@
+package ca.umanitoba.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class LC47PermutationsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, result, new boolean[nums.length], new ArrayDeque<>());
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, boolean[] used, Deque<Integer> path) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            path.addLast(nums[i]);
+            backtrack(nums, result, used, path);
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+}

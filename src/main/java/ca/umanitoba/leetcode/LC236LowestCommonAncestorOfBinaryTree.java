41ad834cb3a3e7927fd5066f6728c1a4b992411c
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-07-16 10:07:54 -0500

    236

---
+++ src/main/java/ca/umanitoba/leetcode/LC236LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,13 @@
+package ca.umanitoba.leetcode;
+
+import ca.umanitoba.datastructures.TreeNode;
+
+public class LC236LowestCommonAncestorOfBinaryTree {
+    public TreeNode lowestCommonAncestorRecursive(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}

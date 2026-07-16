3b8c59d4bbe85cb07cf309a577f6c8c9bf43da55
xiaoran-MENG <meng.xiaoran@outlook.com>
2026-07-16 10:08:27 -0500

    236

--- src/main/java/ca/umanitoba/leetcode/LC236LowestCommonAncestorOfBinaryTree.java
+++ src/main/java/ca/umanitoba/leetcode/LC236LowestCommonAncestorOfBinaryTree.java
@@ -1,13 +1,39 @@
 package ca.umanitoba.leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import ca.umanitoba.datastructures.TreeNode;
 
 public class LC236LowestCommonAncestorOfBinaryTree {
     public TreeNode lowestCommonAncestorRecursive(TreeNode root, TreeNode p, TreeNode q) {
-        
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestorRecursive(root.left, p, q);
+        TreeNode right = lowestCommonAncestorRecursive(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        return right;
     }
 
-    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+    private Map<TreeNode, TreeNode> nodeToParent = new HashMap<>();
 
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+        mapNodeToParent(root, null);
+        Set<TreeNode> parents = new HashSet<>();
+        while (p != null) {
+            parents.add(p);
+            p = nodeToParent.get(p);
+        }
+        while (!parents.contains(q)) q = nodeToParent.get(q);
+        return q;
+    }
+
+    private void mapNodeToParent(TreeNode node, TreeNode parent) {
+        if (node == null) return;
+        nodeToParent.put(node, parent);
+        mapNodeToParent(node.left, node);
+        mapNodeToParent(node.right, node);
     }
 }
